151eaa5c08ecabef7bf8da779bbccda228834b4d
Anderson Almeida <andersonvsc1998@gmail.com>
2022-12-26 22:20:56 -0300

    rename parameters and add Result in exec

--- src/vm.rs
+++ src/vm.rs
@@ -1,16 +1,13 @@
 #![allow(dead_code)]
 
 use crate::{
-    context::LunarContext,
-    lua::*,
-    stack::LuaStack,
-    state::LunarState,
-    table::{MetaTable, Table},
+    context::LunarContext, lua::*, stack::LuaStack, state::LunarState, table::Table,
     typedef::LunarError,
 };
 use libc::c_void;
 use std::{
-    ffi::{CStr, CString}, rc::Rc,
+    ffi::{CStr, CString},
+    rc::Rc,
 };
 
 #[derive(Debug, Clone)]
@@ -24,6 +21,10 @@ impl Lunar {
         Self {
             state: LunarState::new(),
         }
+    }
+
+    pub(crate) fn get_state(&self) -> &LunarState {
+        &self.state
     }
 
     #[inline]
@@ -55,34 +56,37 @@ impl Lunar {
         }
     }
 
-    pub fn create_static_function(&self, name: &str, function: fn(ctx: LunarContext) -> i32) {
+    pub fn create_static_function(&self, name: &str, fcx: fn(ctx: LunarContext) -> i32) {
         unsafe {
-            let ptr = function as *const ();
+            let ptr = fcx as *const ();
             let code: extern "C" fn(L: lua_State) = std::mem::transmute(ptr);
             lua_pushcclosure(self.state.get(), code as *mut c_void, 0);
             lua_setglobal(self.state.get(), Self::to_const_char(name));
         }
     }
 
-    pub fn create_table(&self, name: &str, global: bool, callback: fn(Table)) {
+    pub fn create_table(&self, name: &str, global: bool, tcx: fn(table: Table)) {
         let ctx = Rc::new(LunarContext::new(self.state.get()));
         let table = Table::new(ctx, name, global);
-        callback(table);
+        tcx(table);
     }
 
-    pub fn create_userdate<T>(&self, name: &str, methods: fn(ctx: MetaTable)) {}
+    //pub fn create_userdate<T>(&self, name: &str, methods: fn(ctx: MetaTable)) {}
 
     #[inline]
-    pub fn exec(&self) {
-        self.pcall(0, 0, 0);
+    pub fn exec(&self) -> Result<(), String>{
+        self.pcall(0, 0, 0)
     }
 
-    fn pcall(&self, nargs: i32, nresults: i32, errfunc: i32) {
+    fn pcall(&self, nargs: i32, nresults: i32, errfunc: i32) -> Result<(), String> {
         unsafe {
             if lua_pcallk(self.state.get(), nargs, nresults, errfunc, 0, 0) > 0 {
-                println!("Lunar: {}.", self.get_error());
+                let err = self.get_error();
+                return Err(format!("Lunar: {err}."));
             }
         }
+
+        return Ok(());
     }
 
     fn get_error(&self) -> String {
@@ -94,7 +98,7 @@ impl Lunar {
 
     fn to_string(&self, stack: LuaStack) -> Result<String, LunarError> {
         unsafe {
-            let str = lua_tolstring(self.state.get(), 1, std::ptr::null_mut());
+            let str = lua_tolstring(self.state.get(), stack.into(), std::ptr::null_mut());
             match CStr::from_ptr(str).to_str() {
                 Ok(str) => Ok(String::from(str)),
                 Err(_) => Err("[Lunar]: Unable to convert 'const char*' to 'String'."),
