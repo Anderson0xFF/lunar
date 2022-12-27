5e225db55a114adfdbdcfd4dd460669fb73d8cc8
Anderson Almeida <andersonvsc1998@gmail.com>
2022-12-26 22:19:47 -0300

    convert LuaStack to i32

--- src/stack.rs
+++ src/stack.rs
@@ -8,3 +8,17 @@ pub enum LuaStack {
     STACK5 = 5,
     STACK6 = 6
 }
+
+
+impl From<LuaStack> for i32 {
+    fn from(value: LuaStack) -> Self {
+        match value {
+            LuaStack::STACK1 => 1,
+            LuaStack::STACK2 => 2,
+            LuaStack::STACK3 => 3,
+            LuaStack::STACK4 => 4,
+            LuaStack::STACK5 => 5,
+            LuaStack::STACK6 => 6, 
+        }
+    }
+}
